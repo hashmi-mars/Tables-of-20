5d8032fa34cfe00a0db09ce81be58aff998f7ea3
Hashmi Muhammad <75092354+hashmi-mars@users.noreply.github.com>
2021-01-17 00:03:12 +0500

    Add files via upload

---
+++ tables_16Jan2021.py
@@ -0,0 +1,9 @@
+table=1
+while table <=20:
+    print('Tables of ', table)
+    mul=1
+    while mul <=10:
+        print(table, '*', mul, '=', table * mul)
+        mul += 1
+        print(' ')
+    table = table + 1
